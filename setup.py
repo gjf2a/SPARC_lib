eb50a8442a993d844d819e29fed2632a34a38f54
gjf2a <ferrer@hendrix.edu>
2021-05-25 16:43:49 -0500

    Another attempted fix.

--- setup.py
+++ setup.py
@@ -7,6 +7,5 @@ setup(
     url='https://github.com/gjf2a/SPARC_lib',
     author='Gabriel Ferrer',
     author_email='ferrer@hendrix.edu',
-    license='Public Domain',
-    packages=['SPARC_lib']
+    license='Public Domain'
 )
