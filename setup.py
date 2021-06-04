52dd714f722df30ef9d02de5c3ecf30bb927cefe
gjf2a <ferrer@hendrix.edu>
2021-06-04 16:14:55 -0500

    Added observation_sum().

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='SPARC_lib',
-    version='0.10.0',
+    version='0.11.0',
     description='Tools supporting the SPARC project at Hendrix',
     url='https://github.com/gjf2a/SPARC_lib',
     author='Gabriel Ferrer',
