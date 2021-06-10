b8e393d58a8095fa36ecd7aa0d939d867509e5b9
gjf2a <ferrer@hendrix.edu>
2021-06-09 22:19:25 -0500

    Patched up make_interval_label().

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='SPARC_lib',
-    version='0.17.0',
+    version='0.17.1',
     description='Tools supporting the SPARC project at Hendrix',
     url='https://github.com/gjf2a/SPARC_lib',
     author='Gabriel Ferrer',
