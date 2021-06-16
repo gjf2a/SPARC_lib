71813653aa53a8fff6535fce29ed4960e57923e6
gjf2a <ferrer@hendrix.edu>
2021-06-15 22:10:15 -0500

    Added DANA to activity course list.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='SPARC_lib',
-    version='0.23.2',
+    version='0.23.3',
     description='Tools supporting the SPARC project at Hendrix',
     url='https://github.com/gjf2a/SPARC_lib',
     author='Gabriel Ferrer',
